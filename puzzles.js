c29827a572a6404c75817897b00fc40ba221e760
JimmyDub010 <67116651+JimmyDub010@users.noreply.github.com>
2025-07-28 14:13:08 -0400

    Added new puzzle category and a few puzzles
    enjoy

--- puzzles.js
+++ puzzles.js
@@ -1,6 +1,10 @@
 
 const puzzlesData = [
     { category: "Thing", puzzle: "BASEBALL" },
+    { category: "Thing", puzzle: "big balls" },
+    { category: "Audiogames", puzzle: "Swamp" },
+    { category: "Audiogames", puzzle: "The Killer" },
+    { category: "Audiogames", puzzle: "Survive The Wild" },
     { category: "Phrase", puzzle: "WHEEL OF FORTUNE" },
     { category: "Food and Drink", puzzle: "CHEESE AND CRACKERS" }
 ];
