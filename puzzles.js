bc02f067557de9d86c278fe620aac4faaa801a45
JimmyDub010 <67116651+JimmyDub010@users.noreply.github.com>
2025-07-28 14:49:39 -0400

    new puzzles
    added 15 new puzzles to puzzles.js

--- puzzles.js
+++ puzzles.js
@@ -6,7 +6,20 @@ const puzzlesData = [
     { category: "Audiogames", puzzle: "The Killer" },
     { category: "Audiogames", puzzle: "Survive The Wild" },
     { category: "Phrase", puzzle: "WHEEL OF FORTUNE" },
-    { category: "Phrase", puzzle: "I like big balls" },
-
+    { category: "Thing", puzzle: "TELEPHONE CHARGER" },
+    { category: "Place", puzzle: "GRAND CANYON" },
+    { category: "Phrase", puzzle: "BETTER LATE THAN NEVER" },
+    { category: "Food and Drink", puzzle: "SPAGHETTI AND MEATBALLS" },
+    { category: "Movie Title", puzzle: "JURASSIC PARK" },
+    { category: "Person", puzzle: "ALBERT EINSTEIN" },
+    { category: "Event", puzzle: "SUMMER OLYMPICS" },
+    { category: "Phrase", puzzle: "ONCE IN A BLUE MOON" },
+    { category: "Thing", puzzle: "REMOTE CONTROL" },
+    { category: "Song Title", puzzle: "STAIRWAY TO HEAVEN" },
+    { category: "Place", puzzle: "EMPIRE STATE BUILDING" },
+    { category: "Before and After", puzzle: "BABY SHOWER CURTAIN" },
+    { category: "Occupation", puzzle: "AIRLINE PILOT" },
+    { category: "Animal", puzzle: "KANGAROO" },
+    { category: "Food and Drink", puzzle: "PEANUT BUTTER COOKIES" }
     { category: "Food and Drink", puzzle: "CHEESE AND CRACKERS" }
 ];
