7c7fdef01c65ea50afb1185c7b4bfe405c380fa7
JimmyDub010 <67116651+JimmyDub010@users.noreply.github.com>
2025-08-17 14:21:26 -0400

    changed the entire website, removed the html wheel of fortune, since python rewrite is coming

--- puzzles - Copy.js
+++
@@ -1,6 +0,0 @@
-
-const puzzlesData = [
-    { category: "Thing", puzzle: "BASEBALL" },
-    { category: "Phrase", puzzle: "WHEEL OF FORTUNE" },
-    { category: "Food and Drink", puzzle: "CHEESE AND CRACKERS" }
-];
